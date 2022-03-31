9d63429b08b191cb4d911d955e98d05c01a9f1a6
Jodu555 <Jodu505@gmail.com>
2022-03-31 14:12:12 +0200

    Copyed some stuff together

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const express = require('express');
+const https = require('https');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const dotenv = require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(express.json());
+
+let server;
+if (process.env.https) {
+    const sslProperties = {
+        key: fs.readFileSync(process.env.KEY_FILE),
+        cert: fs.readFileSync(process.env.CERT_FILE),
+    };
+    server = https.createServer(sslProperties, app)
+} else {
+    server = http.createServer(app);
+}
+
+
+// Your Middleware handlers here
+
+const audioFile = '';
+
+app.get('/radio', (req, res) => {
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+    const videoSize = fs.statSync("Chris-Do.mp4").size;
+    const CHUNK_SIZE = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "audio/mp3",
+    };
+    res.writeHead(206, headers);
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+});
+
+
+const PORT = process.env.PORT || 3100;
+server.listen(PORT, () => {
+    console.log(`Express App Listening ${process.env.https ? 'with SSL ' : ''}on `);
+});
